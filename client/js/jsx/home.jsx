a4faf8b84d526ae365fd2212e4d5f68ebd1eaf23
KMS\thaohuynh <thaohuynh@kms-technology.com>
2017-06-14 18:03:05 +0700

    Apply JEST but not finish

--- client/js/jsx/home.jsx
+++ client/js/jsx/home.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import WheelChoice from './components/wheel_choice.jsx';
 
- 
 export default class MainPage extends React.Component {
   render() {
     return (
